7f73135cadba8d47f0dab99fbce03750f24a96fd
zenalex <zenkov@nsgsoft.ru>
2022-01-25 13:50:47 +0300

    доп

--- lib/model/generated/DataController.g.dart
+++ lib/model/generated/DataController.g.dart
@@ -2,27 +2,25 @@ import 'package:get/get.dart';
 import 'package:nsg_data/nsg_data.dart';
 import '../dataControllerModel.dart';
 
-class DataControllerGenerated extends GetxController
-    with StateMixin<NsgBaseControllerData> {
-  NsgDataProvider provider;
+class DataControllerGenerated extends NsgBaseController {
+  NsgDataProvider? provider;
   @override
   void onInit() async {
     if (provider == null) {
-      provider = NsgDataProvider();
-      provider.serverUri = 'http://alex.nsgsoft.ru:5073';
+      provider = NsgDataProvider(firebaseToken: '');
+      provider!.serverUri = 'http://alex.nsgsoft.ru:5073';
       NsgDataClient.client
-       .registerDataItem(PassData(), remoteProvider: provider);
-      await provider.connect();
-      if (provider.isAnonymous) {
-        await Get.to(NsgPhoneLoginPage(provider,
-          widgetParams: NsgPhoneLoginParams.defaultParams))
-          .then((value) => loadData());
+          .registerDataItem(PassData(), remoteProvider: provider);
+      provider!.useNsgAuthorization = false;
+      await provider!.connect(this);
+      if (provider!.isAnonymous) {
+        await Get.to(provider!.loginPage)?.then((value) => loadData());
       } else {
         await loadData();
       }
     }
     super.onInit();
   }
-  
+
   Future loadData() async {}
 }
